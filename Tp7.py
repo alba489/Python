b03d11908730651534b68d4ada5286c28fd28dbf
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-21 08:55:23 -0300

    Add files via upload

---
+++ Tp7.py
@@ -0,0 +1,13 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio 7")
+print("-------------------------------")
+#Ingreso de datos
+N1=int (input("Ingrese el primer nemero "))
+N2=int (input("Ingrese el segundo numero "))
+#Proceso
+S=N1+N2
+P=S/2
+#Solida
+print ("El promedio es ", P)
