5001bd1650f8462776c50419f341363845a7a85d
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:15:12 -0300

    Tp Complementario 6

---
+++ TpC6.py
@@ -0,0 +1,13 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 6")
+print("-------------------------------")
+#Constante
+Milla=1.609344
+#Ingreso de datos
+m=int (input("Ingrese las millas "))
+#Proceso
+k=m*Milla
+#Solida
+print (m," millas tiene ",k," kilometros ")
