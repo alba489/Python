bbdae57dcf230e6f8ed25f02a569433de2b6c3c4
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:07:46 -0300

    Tp Complementario 4

---
+++ TpC4.py
@@ -0,0 +1,12 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 4")
+print("-------------------------------")
+#Ingreso de datos
+T=int (input("Ingrese el tiempo en segundos "))
+M=int (input("Ingrese la distancia en metros "))
+#Proceso
+V=M/T
+#Solida
+print ("La velocidad es ",V,"M/S" )
