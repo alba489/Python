561b9789761231e467acbd4ccec381b626dc8fe4
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:06:51 -0300

    Tp Complementario 2

---
+++ TpC2.py
@@ -0,0 +1,11 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 2")
+print("-------------------------------")
+#Ingreso de datos
+H=int (input("Ingrese la altura de el triangulo "))
+#Proceso
+P=3*(2*H/3**0.5)
+#Solida
+print ("El Perimetro del triangulo es ", P)
