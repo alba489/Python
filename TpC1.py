763f0b6dd70d2901ec9e41c1b4814fde9cc1be3a
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:06:27 -0300

    Tp Complementario 1

---
+++ TpC1.py
@@ -0,0 +1,13 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 1")
+print("-------------------------------")
+#Constante
+PI=3.1416
+#Ingreso de datos
+R=int (input("Ingrese el radio "))
+#Proceso
+V=4/3*PI*R**3
+#Solida
+print ("El Volumen de la esfera es ", V)
