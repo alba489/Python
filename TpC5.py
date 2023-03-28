f4f37658e48c45fd9741469d3ce5783f5f41351d
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:08:36 -0300

    Tp Complementario 5

---
+++ TpC5.py
@@ -0,0 +1,12 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 5")
+print("-------------------------------")
+#Ingreso de datos
+c=float (input("Ingrese el costo del articulo "))
+d=int (input("Ingrese el numero de docenas "))
+#Proceso
+P=d*12*c
+#Solida
+print ("El precio del articulo es ",P)
