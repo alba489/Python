779ab6b16ec260000bc93b025a4603ba40101b4a
alba489 <127850077+alba489@users.noreply.github.com>
2023-03-28 11:07:16 -0300

    Tp Complementario 3

---
+++ TpC3.py
@@ -0,0 +1,16 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 3")
+print("-------------------------------")
+#Constante
+EURO=3.83
+DOLAR=3.25
+#Ingreso de datos
+SOLES=int (input("Ingrese su cantidad de soles "))
+#Proceso
+D=SOLES/DOLAR
+E=SOLES/EURO
+#Solida
+print ("En ",SOLES," soles hay ",E,"euros" )
+print ("En ",SOLES," soles hay ",D,"dolares" )
