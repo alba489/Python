9083ba962306fdc4e379934a7ec79795b4a39920
alba489 <127850077+alba489@users.noreply.github.com>
2023-04-04 09:08:25 -0300

    Tp Complementario 9

---
+++ TpC9.py
@@ -0,0 +1,15 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 9")
+print("-------------------------------")
+#Constante
+PI=3.1416
+#Ingreso de datos
+x=int (input("Ingrese angulos radianes "))
+#Proceso
+sex=x*180/PI
+cen=x*200/PI
+#Solida
+print("En sexagesimales es:", sex)
+print("En centesimales es:", cen)
