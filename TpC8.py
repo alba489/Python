8090336a657f43ccff5f65af28317fcba279c79d
alba489 <127850077+alba489@users.noreply.github.com>
2023-04-04 09:08:01 -0300

    Tp Complementario 8

---
+++ TpC8.py
@@ -0,0 +1,13 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio Complementario 8")
+print("-------------------------------")
+#Ingreso de datos
+va=int (input("Ingrese la velocidad del cuerpo .a "))
+vb=int (input("Ingrese la velocidad del cuerpo .b "))
+D=float (input("Ingrese la distancia que los separa "))
+#Proceso
+te=D/(va+vb)
+#Solida
+print("Los cuerpos se encuentran en: ",te," segundos")
