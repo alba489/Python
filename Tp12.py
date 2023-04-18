80658e0da3e141d318e9b716a56df3155cd63391
alba489 <127850077+alba489@users.noreply.github.com>
2023-04-18 16:05:29 +0200

    Tp12

---
+++ Tp12.py
@@ -0,0 +1,11 @@
+print("-------------------------------------------------------")
+print("Ejercicio12")
+print("-------------------------------------------------------")
+#Entradas
+SUE = float( input("Ingrese Su Sueldo: "))
+#Salida
+print("-------------------------------------------------------")
+if SUE < 1000 :
+    AMU=SUE*0.15
+    SUE=SUE+AMU
+print("El sueldo es: ", SUE)
